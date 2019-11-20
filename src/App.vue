3b1042744d9cac77602cd4e00cb31624a4d9196d
Арсен <arsennaimov@gmail.com>
2019-11-20 15:15:03 +0300

    restructuring

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div align="center" id="app">
-    <PlayingField v-bind:game="game" v-bind:gameIsStarted="gameIsStarted" />
+    <PlayingField v-bind:gameIsStarted="gameIsStarted" :field="field" />
 
     <div v-if="this.game.playingFieldSize < 1">
       <h2>Добрейший вечерочек</h2>
@@ -10,11 +10,7 @@
       </label>
     </div>
 
-    <ControlButtons
-      v-bind:startGame="startGame"
-      v-bind:stopGame="stopGame"
-      v-if="this.game.playingFieldSize > 9"
-    />
+    <ControlButtons v-bind:startGame="startGame" v-bind:stopGame="stopGame" v-if="this.field" />
   </div>
 </template>
 
@@ -31,9 +27,8 @@ export default {
   },
   data: function() {
     return {
+      field: [], //игровое поле
       inputFieldSize: null, //размер игрового поля, введенный пользователем
-      oldGenerationCells: [], //старое поколение ячеек
-      newGenerationCells: [], //новое поколение ячеек
       gameIsStarted: false, //проверка на запуск игры
       game: {
         playingFieldSize: 0,
@@ -44,50 +39,31 @@ export default {
   methods: {
     startGame: function() {
       this.gameIsStarted = true;
-      for (let i = 0; i < this.game.cells.length; i++) {
-        this.game.cells[i].life = true;
-      }
 
-      for (let i = 0; i < this.oldGenerationCells.length; i++) {
-        document.getElementById(
-          this.oldGenerationCells[i].y + "," + this.oldGenerationCells[i].x
-        ).bgColor = ""; //чистка поля от мертвых клеток
-      }
+      this.game.cells = [];
+      this.field.forEach(row => {
+        row.forEach(cell => {
+          if (cell.life) this.game.cells.push(cell);
+        });
+      });
 
-      this.newGenerationCells = [];
       axios
         .post("http://localhost:8075/game", this.game)
         .then(response => {
-          this.game.playingFieldSize = response.data.playingFieldSize;
+          this.game = response.data;
 
-          for (let i = 0; i < response.data.cells.length; i++) {
-            let newGenerationCell = {
-              y: response.data.cells[i].y,
-              x: response.data.cells[i].x
-            };
-            this.newGenerationCells.push(newGenerationCell);
+          this.field = this.newFields(this.game);
+
+          if (this.gameIsStarted && this.game.cells.length > 0) {
+            this.game.cells = [];
+            setTimeout(this.startGame, 10);
+            this.game.cells = [];
           }
-
-          for (let y = 1; y <= this.game.playingFieldSize; y++) {
-            for (let x = 1; x <= this.game.playingFieldSize; x++) {
-              document.getElementById(y + "," + x).bgColor = "";
-            }
-          }
-
-          for (var i = 0; i < this.newGenerationCells.length; i++)
-            document.getElementById(
-              this.newGenerationCells[i].y + "," + this.newGenerationCells[i].x
-            ).bgColor = "black";
-          this.game.cells = this.newGenerationCells;
-
-          if (this.game.cells.length > 0 && this.gameIsStarted === true)
-            setTimeout(this.startGame, 100);
         })
         .catch(error => {
           /* eslint-disable no-console */
           console.log(error);
           /* eslint-enable no-console */
-
           alert(
             "При ожидании данных с сервера что-то пошло не так, как должно..."
           );
@@ -96,41 +72,54 @@ export default {
 
     stopGame: function() {
       this.gameIsStarted = false;
-      this.newGenerationCells = [];
+    },
 
-      for (let y = 1; y <= this.game.playingFieldSize; y++) {
-        for (let x = 1; x <= this.game.playingFieldSize; x++) {
-          document.getElementById(y + "," + x).bgColor = "";
+    //функция для создания новых таблиц
+    newFields: function(newGame) {
+      let newGenerationField = [];
+      for (let y = 1; y <= newGame.playingFieldSize; y++) {
+        newGenerationField.push([]);
+        for (let x = 1; x <= newGame.playingFieldSize; x++) {
+          newGenerationField[y - 1].push({
+            y: y,
+            x: x,
+            life: false
+          });
         }
       }
-      this.game.cells = [];
+
+      newGame.cells.forEach(cell => {
+        newGenerationField[cell.y - 1][cell.x - 1].life = true;
+      });
+
+      this.field = newGenerationField;
+      return newGenerationField;
     }
   },
 
   watch: {
     inputFieldSize: function(val) {
       val = parseInt(val, 10);
-      val > 19 && val < 51
-        ? (this.game.playingFieldSize = val)
-        : alert("Введите значение от 20 до 50");
+      if (val > 19 && val < 51) {
+        this.game.playingFieldSize = val;
+
+        this.field = this.newFields(this.game);
+      } else {
+        alert("Введите значение от 20 до 50");
+      }
     }
   },
 
   created: function() {
     axios.get("http://localhost:8075/game").then(response => {
       if (response.data !== "") {
-        this.game.playingFieldSize = response.data.playingFieldSize;
-        for (let i = 0; i < response.data.cells.length; i++) {
-          let cell = {
-            y: response.data.cells[i].y,
-            x: response.data.cells[i].x
-          };
-          this.game.cells.push(cell);
-        }
+        this.game = response.data;
+
+        this.field = this.newFields(this.game);
+
         this.startGame();
       }
     });
   }
 };
 </script>
-
--- src/components/app/PlayingField.vue
+++ src/components/app/PlayingField.vue
@@ -1,43 +1,24 @@
 <template>
 <table cellspacing="0">
-                <tr v-for="column in game.playingFieldSize" v-bind:key="column.id">
-                    <td  class="square" :id="[column,line]" v-for="line in game.playingFieldSize" v-bind:key="line.id"  v-on:mouseover="selectCell"></td>
+                <tr v-for="column in field" v-bind:key="column.id">
+                    <td :class="{ life: cell.life }" v-for="cell in column" :key="cell.id"  @mouseover="cell.life = !cell.life"></td>
                 </tr>
 </table>
 </template>
 
 <script>
 export default {
-     props: ['gameIsStarted', 'game'],
-     methods: {
-        selectCell: function (event) {
-     
-            if (this.gameIsStarted === true) {
-                return;
-            }
-            if (document.getElementById(event.target.id).bgColor) {
-                document.getElementById(event.target.id).bgColor = "";
-                this.game.cells.splice(this.game.cells.indexOf({
-                    y: event.target.id.split(",")[0],
-                    x: event.target.id.split(",")[1]
-                }), 1)
-            } else {
-                document.getElementById(event.target.id).bgColor = "black";
-                this.game.cells.push({
-                    y: event.target.id.split(",")[0],
-                    x: event.target.id.split(",")[1]
-                });
-            }
-
-        }
-    }
+    props: ['gameIsStarted', 'game', 'field'],
 }
 </script>
 
 <style scoped>
-td.square {
+td {
   border: solid 1px black;
   width: 10px;
   height: 10px;
+}
+.life{
+    background-color: black;
 }
 </style>
